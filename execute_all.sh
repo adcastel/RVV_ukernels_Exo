fcc2db3c8a4c1f6a2c86e5b7fdd4fa5da974e08a
Adrián Castelló <adcastel@uji.es>
2024-04-16 10:21:36 +0200

    added kc examples

--- execute_all.sh
+++ execute_all.sh
@@ -1,8 +1,26 @@
 #/bin/bash!
 
-#BCAST-NO SWAP
-make && ./test_uk 1 24 1 24 512 0 1000 > ${HOSTNAME}_BCAST.dat 
-make SWAPAB=1 && ./test_uk 1 24 1 24 512 0 1000 > ${HOSTNAME}_BCAST_SWAPAB.dat 
-make GATHER=1 && ./test_uk 1 24 1 24 512 0 1000 > ${HOSTNAME}_GATHER.dat 
-make SWAPAB=1 GATHER=1 && ./test_uk 1 24 1 24 512 0 1000 > ${HOSTNAME}_GATHER_SWAPAB.dat 
+#BCAST-NOSWAP
+for k in 32 64 128 256 512 1024 2048 4096
+do
+make && ./test_uk 1 24 1 24 $k 0 1000 > ${HOSTNAME}_BCAST_$k.dat 
+done
+
+#BCAST-SWAP
+for k in 32 64 128 256 512 1024 2048 4096
+do
+make SWAPAB=1 && ./test_uk 1 24 1 24 $k 0 1000 > ${HOSTNAME}_BCAST_SWAPAB_$k.dat 
+done
+
+#GATHER-NOSWAP
+for k in 32 64 128 256 512 1024 2048 4096
+do
+make GATHER=1 && ./test_uk 1 24 1 24 $k 0 1000 > ${HOSTNAME}_GATHER_$k.dat 
+done
+
+#GATHER-SWAP
+for k in 32 64 128 256 512 1024 2048 4096
+do
+make SWAPAB=1 GATHER=1 && ./test_uk 1 24 1 24 $k 0 1000 > ${HOSTNAME}_GATHER_SWAPAB_$k.dat 
+done
 
